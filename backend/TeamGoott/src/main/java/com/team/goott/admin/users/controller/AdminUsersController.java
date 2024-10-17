c56713e0676ee1e34ee2db94e5e08c5652d63ddd
yhs99 <khb113322@naver.com>
2024-10-17 15:07:02 +0900

    사용자 전체 목록, 특정 사용자 정보 조회 API
    인터셉터 작동여부 테스트 필요함

--- backend/TeamGoott/src/main/java/com/team/goott/admin/users/controller/AdminUsersController.java
+++ backend/TeamGoott/src/main/java/com/team/goott/admin/users/controller/AdminUsersController.java
@@ -1,11 +1,41 @@
 package com.team.goott.admin.users.controller;
 
+import javax.servlet.http.HttpSession;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import com.team.goott.admin.users.service.AdminUsersService;
+import com.team.goott.user.domain.UserDTO;
 
 import lombok.extern.slf4j.Slf4j;
 
 @RestController
+@RequestMapping("/admin")
 @Slf4j
 public class AdminUsersController {
-
+	
+	@Autowired
+	private AdminUsersService ads;
+	
+	// 전체 사용자 목록
+	@GetMapping("/users")
+	public ResponseEntity<Object> getUsers() {
+		return ResponseEntity.ok(ads.getAllUsers());
+	}
+	
+	// 특정 사용자 정보
+	@GetMapping("/users/{userId}")
+	public ResponseEntity<Object> getUserByUserId(HttpSession session, @PathVariable int userId) {
+		UserDTO userInfo = ads.getUserInfo(userId);
+		if(userInfo == null) {
+			return ResponseEntity.status(HttpStatus.NOT_FOUND).body("존재하지 않는 유저입니다.");
+		}
+		return ResponseEntity.ok(userInfo);
+	}
 }
--- backend/TeamGoott/src/main/java/com/team/goott/admin/users/service/AdminUsersServiceImpl.java
+++ backend/TeamGoott/src/main/java/com/team/goott/admin/users/service/AdminUsersServiceImpl.java
@@ -1,11 +1,36 @@
 package com.team.goott.admin.users.service;
 
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+
+import com.team.goott.admin.users.persistence.AdminUsersDAO;
+import com.team.goott.user.domain.UserDTO;
 
 import lombok.extern.slf4j.Slf4j;
 
 @Service
 @Slf4j
 public class AdminUsersServiceImpl implements AdminUsersService {
+	
+	@Autowired
+	private AdminUsersDAO dao;
+	
+	@Override
+	public Map<String, Object> getAllUsers() {
+		List<UserDTO> userLists = dao.getAllUsers();
+		Map<String, Object> returnMap = new HashMap<String, Object>();
+		returnMap.put("userCount", userLists.size());
+		returnMap.put("userLists", userLists);
+		return returnMap;
+	}
+
+	@Override
+	public UserDTO getUserInfo(int userId) {
+		return dao.getUserInfoByUserId(userId);
+	}
 
 }

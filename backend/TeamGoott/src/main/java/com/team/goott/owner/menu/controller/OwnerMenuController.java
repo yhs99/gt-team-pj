02c34feb8a2ea37ba4fcc72b3d8b963f86f46c49
lee <hyunmyoung266@gmail.com>
2024-10-17 13:08:52 +0900

    init

--- backend/TeamGoott/src/main/java/com/team/goott/owner/menu/controller/OwnerMenuController.java
+++ backend/TeamGoott/src/main/java/com/team/goott/owner/menu/controller/OwnerMenuController.java
@@ -1,11 +1,26 @@
 package com.team.goott.owner.menu.controller;
 
+import javax.inject.Inject;
+
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import com.team.goott.owner.menu.service.OwnerMenuService;
 
 import lombok.extern.slf4j.Slf4j;
 
 @RestController
 @Slf4j
 public class OwnerMenuController {
+	
+	@Inject
+	OwnerMenuService service;
+	
+	@GetMapping("/menu")
+	public ResponseEntity<Object> getMenu(){
+		log.info("menuAPI 호출");
+		return null;
+	}
 
 }

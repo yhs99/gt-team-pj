0312694961fad7808d044468dd62ba1b1bb44657
minsung12345 <whss5699@naver.com>
2024-10-21 11:08:13 +0900

    식당 정보 조회 끝 확인 부탁드립니다.

--- backend/TeamGoott/src/main/java/com/team/goott/user/store/persistence/UserStoreDAO.java
+++ backend/TeamGoott/src/main/java/com/team/goott/user/store/persistence/UserStoreDAO.java
@@ -1,5 +1,11 @@
 package com.team.goott.user.store.persistence;
 
+import java.util.List;
+
+import com.team.goott.user.domain.StoreDTO;
+
 public interface UserStoreDAO {
+
+	List<StoreDTO> getAllStores() throws Exception;
 
 }
--- backend/TeamGoott/src/main/java/com/team/goott/user/store/service/UserStoreServiceImpl.java
+++ backend/TeamGoott/src/main/java/com/team/goott/user/store/service/UserStoreServiceImpl.java
@@ -1,11 +1,26 @@
 package com.team.goott.user.store.service;
 
+import java.util.List;
+
+import javax.inject.Inject;
+
 import org.springframework.stereotype.Service;
+
+import com.team.goott.user.domain.StoreDTO;
+import com.team.goott.user.store.persistence.UserStoreDAO;
 
 import lombok.extern.slf4j.Slf4j;
 
 @Service
 @Slf4j
 public class UserStoreServiceImpl implements UserStoreService {
+	
+	@Inject
+	private UserStoreDAO userStoreDAO;
+	
+	@Override
+	public List<StoreDTO> getAllStores() throws Exception {
+		return userStoreDAO.getAllStores();
+	}
 
 }

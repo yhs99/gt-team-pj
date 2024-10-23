36fe4db5015fbdc0f51604bfd38ce42435fff928
bumjun12 <kingdomtwo9630@gmail.com>
2024-10-23 12:01:57 +0900

    서비스단 DAO객체 주입수정

--- backend/TeamGoott/src/main/java/com/team/goott/owner/coupon/service/OwnerCouponServiceImpl.java
+++ backend/TeamGoott/src/main/java/com/team/goott/owner/coupon/service/OwnerCouponServiceImpl.java
@@ -4,6 +4,7 @@ import java.time.LocalDateTime;
 import java.time.format.DateTimeFormatter;
 import java.util.List;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
@@ -17,6 +18,7 @@ import lombok.extern.slf4j.Slf4j;
 @Slf4j
 public class OwnerCouponServiceImpl implements OwnerCouponService {
 
+		@Autowired
 	    private OwnerCouponDAO ownerCouponDAO;
 
 	    // 쿠폰 조회

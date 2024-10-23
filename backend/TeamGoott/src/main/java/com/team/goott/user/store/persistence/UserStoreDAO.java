9e1e19bcd831fd911783121106430d01c732e001
minsung12345 <whss5699@naver.com>
2024-10-23 11:37:50 +0900

    식당 정보 조회 끝 확인 부탁드립니다. (#68)
    * 식당 정보 조회 끝 확인 부탁드립니다.
    
    * 1차 끝 확인 부탁합니다.
    
    * inject 변경 했습니다.
    
    * 밑줄제거, 낫파운드 오케이로 변경
    
    ---------
    
    Co-authored-by: yhs99 <45925957+yhs99@users.noreply.github.com>

--- backend/TeamGoott/src/main/java/com/team/goott/user/store/persistence/UserStoreDAO.java
+++ backend/TeamGoott/src/main/java/com/team/goott/user/store/persistence/UserStoreDAO.java
@@ -1,5 +1,15 @@
 package com.team.goott.user.store.persistence;
 
+import java.util.List;
+
+import com.team.goott.user.domain.StoreDTO;
+
 public interface UserStoreDAO {
+
+	List<StoreDTO> getAllStores() throws Exception;
+
+	List<Object> getStoreById(int storeId) throws Exception;
+
+	List<StoreDTO> getStoresByCategoriesAndSidos(List<String> categoryCodeIds, List<Integer> sidoCodeIds) throws Exception;
 
 }

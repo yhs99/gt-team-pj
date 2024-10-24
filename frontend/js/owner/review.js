8a5e2122313ce36e2f9089afe39fa993f734ad0e
Hyunmyoung Lee <hyunmyoung266@gmail.com>
2024-10-24 17:46:23 +0900

    commit before merge main branch

--- frontend/js/owner/review.js
+++ frontend/js/owner/review.js
@@ -10,33 +10,30 @@ new Vue({
     },
 
     methods : {
-        saveStoreDTO(){
-            const storeDTO = {
-                storeId: 3,
-                ownerId: 101,
-                rotationId: 5,
-                sidoCodeId: 32,
-                address: "123 Main St, City, Country",
-                tel: "123-456-7890",
-                description: "A great place to eat.",
-                directionGuide: "Next to the big mall.",
-                maxPeople: 50,
-                maxPeoplePerReserve: 10,
-                locationLatX: 37.123456,
-                locationLonY: 127.123456
-            };
+        // saveStoreDTO(){
+        //     const storeDTO = {
+        //         storeId: 3,
+        //         ownerId: 101,
+        //         rotationId: 5,
+        //         sidoCodeId: 32,
+        //         address: "123 Main St, City, Country",
+        //         tel: "123-456-7890",
+        //         description: "A great place to eat.",
+        //         directionGuide: "Next to the big mall.",
+        //         maxPeople: 50,
+        //         maxPeoplePerReserve: 10,
+        //         locationLatX: 37.123456,
+        //         locationLonY: 127.123456
+        //     };
 
-            // sessionStorage.setItem('store',JSON.stringify(storeDTO));
-
-            // const storedStoreDTO = JSON.parse(sessionStorage.getItem('store'));
-           return  axios.post("/api/owner/review", storeDTO)
-            .then(response =>{
-                console.log('storeDTO saved successfully : ', response.data)
-            })
-            .catch(error =>{
-                console.error('Error :' , error);
-            });
-        },
+        //    return  axios.post("/api/owner/review", storeDTO)
+        //     .then(response =>{
+        //         console.log('storeDTO saved successfully : ', response.data)
+        //     })
+        //     .catch(error =>{
+        //         console.error('Error :' , error);
+        //     });
+        // },
 
         getAllReviews(){
             axios.get("/api/owner/review")
@@ -53,9 +50,7 @@ new Vue({
         },
     },
     mounted(){
-        this.saveStoreDTO().then(() => {
-            this.getAllReviews();
-        });
+        this.getAllReviews();
     }
 })
 

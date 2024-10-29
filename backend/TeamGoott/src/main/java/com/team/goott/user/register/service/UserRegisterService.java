9f1394b51555593a67a49602e257ab6b45b16a0d
kimdason <spfhht@naver.com>
2024-10-29 22:07:57 +0900

    회원 정보 조회/수정(이름,전화번호,프로필 이미지) (#87)
    Co-authored-by: yhs99 <45925957+yhs99@users.noreply.github.com>

--- backend/TeamGoott/src/main/java/com/team/goott/user/register/service/UserRegisterService.java
+++ backend/TeamGoott/src/main/java/com/team/goott/user/register/service/UserRegisterService.java
@@ -1,8 +1,15 @@
 package com.team.goott.user.register.service;
 
+import org.springframework.web.multipart.MultipartFile;
+
 import com.team.goott.infra.ValidationException;
+import com.team.goott.user.domain.UserDTO;
 import com.team.goott.user.register.domain.UserRegisterDTO;
 
 public interface UserRegisterService {
 	public int userRegister(UserRegisterDTO user) throws ValidationException, Exception;
+
+	public UserDTO userInfo(int userId);
+
+	public int userUpdate(UserDTO userDTO, MultipartFile imageFile) throws ValidationException, Exception;
 }

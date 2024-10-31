408b31bd2f33de2f59b214d649dfcdc99f540787
Hyunmyoung Lee <hyunmyoung266@gmail.com>
2024-10-31 18:22:06 +0900

    10/31 owner/index.html 1차 수정

--- frontend/js/owner/index.js
+++ frontend/js/owner/index.js
@@ -1,15 +1,46 @@
+
 new Vue({
   el: '#app', // Vue 인스턴스를 '#app'에 바인딩
   data: {
     name:"",
     loginYN:false,
-    reviews:[]
+    reviews:[],
+    reserves:[]
   },
   created: function() {
     this.fetchUserData(); // Vue 인스턴스 생성 시 데이터 요청
     this.fetchReviews();
+    this.getFullCalendar();
+    this.fetchReserves();
   },
   methods: {
+    getFullCalendar(){
+      document.addEventListener('DOMContentLoaded', function() {
+        var calendarEl = document.getElementById('calendar');
+        var calendar = new FullCalendar.Calendar(calendarEl, {
+          initialView: 'dayGridMonth'
+        });
+        calendar.render();
+      });
+    },
+    
+    fetchReserves : function(){
+      axios.get('/api/owner/reserve')
+        .then(response =>{
+          console.log(response);
+          this.reserves = response.data.data.reservations;
+        })
+    },
+
+    updateReserveStatus : function(reserveId, status){
+      var statusCode = Number.parseInt(status);
+      const params = {statusCode : statusCode}
+      axios.post('/api/owner/reserve/'+ reserveId,{params})
+        .then((response =>{
+          console.log(response);
+        }))
+    },
+
     fetchUserData: function() {
       axios.get('/api/status') // 서버에 /api/status 요청
       .then(response => {
@@ -40,11 +71,11 @@ new Vue({
       axios.get('/api/owner/review')
       .then(response => {
         console.log(response);
-        this.reviews = response.data.data.reviews
+        this.reviews = response.data.data.reviews;
       })
       .catch(error => {
         console.log(error);
-      })
+      });
     },
     deleteReq: function(reviewId) {
       axios.delete('/api/owner/review/'+reviewId)

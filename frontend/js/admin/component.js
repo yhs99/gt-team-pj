f69b7fa4eb4cf67e5a18c64d856dc72bc593c515
yhs99 <khb113322@naver.com>
2024-11-02 20:27:21 +0900

    가게 수정 화면, 기능 개발

--- frontend/js/admin/component.js
+++ frontend/js/admin/component.js
@@ -95,7 +95,7 @@ Vue.component('spinner-component', {
   template: `
   <div
         id="spinner"
-        class="show bg-white position-fixed translate-middle w-100 vh-100 top-50 start-50 d-flex align-items-center justify-content-center">
+        class="show position-fixed translate-middle w-100 vh-100 top-50 start-50 d-flex align-items-center justify-content-center">
         <div class="spinner-border text-primary" style="width: 3rem; height: 3rem" role="status">
           <span class="sr-only">Loading...</span>
         </div>

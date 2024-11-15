3c349aa04ad0baa60ad8a86a4a3766f687e5dbd3
yhs99 <khb113322@naver.com>
2024-11-15 21:46:28 +0900

    리뷰 api 코드 리팩토링

--- backend/TeamGoott/src/main/java/com/team/goott/user/review/persistence/UserReviewDAO.java
+++ backend/TeamGoott/src/main/java/com/team/goott/user/review/persistence/UserReviewDAO.java
@@ -9,45 +9,37 @@ import com.team.goott.user.domain.ReviewPageDTO;
 import com.team.goott.user.domain.ReviewImagesDTO;
 
 public interface UserReviewDAO {
-
-
-List<ReviewDTO> getAllReviews(ReviewPageDTO paging , String sort);
-
-ReviewDTO reviewByNo(int reviewId);
-
-int insertReview(ReviewDTO reviewDTO);
-
-int delReview(int reviewId);
-
-int insertImgs(ReviewImagesDTO reviewImg);
-
-List<ReviewImagesDTO> filesByNo(int reviewId);
-
-int delFiles(int reviewId);
-
-int updateReview(ReviewDTO reviewDTO);
-
-void deleteImgs(int imageId);
-
-List<ReviewDTO> getMyReviews(ReviewPageDTO paging);
-
-List<ReserveDTO> getReserveByUserId(int UserId);
-
-List<ReviewDTO> getUserReviews(int userId);
-
-ReserveDTO getReserveInfo(int reserveId);
-
-int checkIfImageExist(int imageId);
-
-int changeStatusCodeId(int reserveId, int statusCode);
-
-ReviewDTO selectUserByUserId(int userId);
-
+	List<ReviewDTO> getAllReviews(ReviewPageDTO paging , String sort);
+	
+	ReviewDTO reviewByNo(int reviewId);
+	
+	int insertReview(ReviewDTO reviewDTO);
+	
+	int delReview(int reviewId);
+	
+	int insertImgs(ReviewImagesDTO reviewImg);
+	
+	List<ReviewImagesDTO> filesByNo(int reviewId);
+	
+	int delFiles(int reviewId);
+	
+	int updateReview(ReviewDTO reviewDTO);
+	
+	void deleteImgs(int imageId);
+	
+	List<ReviewDTO> getMyReviews(ReviewPageDTO paging);
+	
+	List<ReserveDTO> getReserveByUserId(int UserId);
+	
+	List<ReviewDTO> getUserReviews(int userId);
+	
+	ReserveDTO getReserveInfo(int reserveId);
+	
 	int checkIfImageExist(int imageId);
-
+	
 	int changeStatusCodeId(int reserveId, int statusCode);
+	
+	ReviewDTO selectUserByUserId(int userId);
   
 	int setNotification(NotificationDTO notification);
-
-
 }
--- backend/TeamGoott/src/main/java/com/team/goott/user/review/service/UserReviewServiceImpl.java
+++ backend/TeamGoott/src/main/java/com/team/goott/user/review/service/UserReviewServiceImpl.java
@@ -5,12 +5,8 @@ import java.util.ArrayList;
 import java.util.List;
 import java.util.Map;
 
-import javax.inject.Inject;
-
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
-import org.springframework.transaction.annotation.Isolation;
-import org.springframework.transaction.annotation.Propagation;
 import org.springframework.transaction.annotation.Transactional;
 import org.springframework.web.multipart.MultipartFile;
 
@@ -42,36 +38,36 @@ public class UserReviewServiceImpl implements UserReviewService {
   @Autowired
   private UserReviewDAO revDAO;
 
-  @Override
-  public List<ReviewDTO> getAllReviews(int storeId, String sort, int page, int size) {
-    ReviewPageDTO paging = ReviewPageDTO.builder().storeId(storeId).page(page).size(size).build();
-    paging.setStartRow();
-    log.info(paging.toString());
-    List<ReviewDTO> lst = revDAO.getAllReviews(paging , sort);
-    for(ReviewDTO list : lst) {
-      log.info("{}",list.getCreateAtLocalDateTime().toString());
-      List<ReviewImagesDTO> images = selectReviewImagesByReviewId(list.getReviewId());
-      list.setReviewImages(images);
-      ReviewDTO userInfo = revDAO.selectUserByUserId(list.getUserId());
-      String userName = userInfo.getName();
-      String profileUrl = userInfo.getProfileImageUrl();
-      list.setName(userName);
-      list.setProfileImageUrl(profileUrl);
-    }
-    return lst;
-  }
+	  @Override
+	  public List<ReviewDTO> getAllReviews(int storeId, String sort, int page, int size) {
+	    ReviewPageDTO paging = ReviewPageDTO.builder().storeId(storeId).page(page).size(size).build();
+	    paging.setStartRow();
+	    log.info(paging.toString());
+	    List<ReviewDTO> lst = revDAO.getAllReviews(paging , sort);
+	    for(ReviewDTO list : lst) {
+	      log.info("{}",list.getCreateAtLocalDateTime().toString());
+	      List<ReviewImagesDTO> images = selectReviewImagesByReviewId(list.getReviewId());
+	      list.setReviewImages(images);
+	      ReviewDTO userInfo = revDAO.selectUserByUserId(list.getUserId());
+	      String userName = userInfo.getName();
+	      String profileUrl = userInfo.getProfileImageUrl();
+	      list.setName(userName);
+	      list.setProfileImageUrl(profileUrl);
+	    }
+	    return lst;
+	  }
 
 
-  @Override
-  public ReviewDTO reviewByNo(int reviewId) {
-    // 리뷰 조회
-    ReviewDTO review = revDAO.reviewByNo(reviewId);
-    List<ReviewImagesDTO> imagesDTO = new ArrayList<ReviewImagesDTO>();
-    imagesDTO = revDAO.filesByNo(reviewId);
-    review.setReviewImages(imagesDTO);
-
-    return review;
-  }
+	@Override
+	public ReviewDTO reviewByNo(int reviewId) {
+		// 리뷰 조회
+		ReviewDTO review = revDAO.reviewByNo(reviewId);
+		List<ReviewImagesDTO> imagesDTO = new ArrayList<ReviewImagesDTO>();
+		imagesDTO = revDAO.filesByNo(reviewId);
+		review.setReviewImages(imagesDTO);
+		
+		return review;
+	}
 
 	@Override
 	@Transactional
@@ -117,72 +113,33 @@ public class UserReviewServiceImpl implements UserReviewService {
 		
 		return revDAO.setNotification(notification);
 	}
-
-@Override
-public ReviewImagesDTO imageIntoDTO(int reviewId, MultipartFile file) throws IOException, Exception {
-	ReviewImagesDTO reviewImg = new ReviewImagesDTO();
-	// 원본 이미지 저장
-	log.info("리뷰아이디 : {}", reviewId);
-	S3ImageManager imageManager = new S3ImageManager(file, s3Client, bucketName);
-	Map<String, String> reviewImages = imageManager.uploadImage();
-	String ext = "";
-	String originalFileName=file.getOriginalFilename();
-
-	if (originalFileName == null || !originalFileName.contains(".")) {
-	    throw new IllegalArgumentException(INVALID_FILENAME);
-	}else {
-		ext = originalFileName.substring(originalFileName.lastIndexOf(".")+1);
-	}
-
-	reviewImg.setUrl(reviewImages.get("imageUrl"));
-	reviewImg.setFileName(reviewImages.get("imageFileName"));
-	reviewImg.setFileType(ext);
-	reviewImg.setReviewId(reviewId);
-
-	log.info("reivewImage DTO : {} ", reviewImg.toString());
-
-	return reviewImg;
-}
-
+	
 	@Override
-	@Transactional
-	public boolean deleteReviewNFile(int reviewId, int reserveId, List<ReviewImagesDTO> list) {
-		// 리뷰 삭제, 파일 삭제
-		boolean result = false;
-		
-		try {
-			if(revDAO.delReview(reviewId) == 1) {
-			 log.info("리뷰 삭제 완료");
-				
-			 if (list != null && !list.isEmpty()) {
-					if(revDAO.delFiles(reviewId) > 0) {
-					  log.info("파일 삭제 정보 삭제 완료");
-	                  log.info("삭제할 이미지 개수: {}", list.size());
-						
-	                  for(ReviewImagesDTO fileName : list) {
-							result = new S3ImageManager(s3Client, bucketName, fileName.getFileName()).deleteImage();
-							if(result) {
-								log.info("파일 삭제 완료 : "+ fileName.getFileName());
-							}
-						}
-					}
-				   }else {
-					   log.info("삭제할 이미지가 없음, 리뷰만 삭제 완료");
-				   }
-			 if(revDAO.changeStatusCodeId(reserveId, 4) > 0){
-					
-				 	result=true;
-					log.info("statusCode 4변경 완료");
-					
-					}
-				}
-			
-			} catch (Exception e) {
-				 log.error("삭제 중 오류 발생: {}", e.getMessage());
-			}
-			
-		return result;
+	public ReviewImagesDTO imageIntoDTO(int reviewId, MultipartFile file) throws IOException, Exception {
+		ReviewImagesDTO reviewImg = new ReviewImagesDTO();
+		// 원본 이미지 저장
+		log.info("리뷰아이디 : {}", reviewId);
+		S3ImageManager imageManager = new S3ImageManager(file, s3Client, bucketName);
+		Map<String, String> reviewImages = imageManager.uploadImage();
+		String ext = "";
+		String originalFileName=file.getOriginalFilename();
+	
+		if (originalFileName == null || !originalFileName.contains(".")) {
+		    throw new IllegalArgumentException(INVALID_FILENAME);
+		}else {
+			ext = originalFileName.substring(originalFileName.lastIndexOf(".")+1);
+		}
+	
+		reviewImg.setUrl(reviewImages.get("imageUrl"));
+		reviewImg.setFileName(reviewImages.get("imageFileName"));
+		reviewImg.setFileType(ext);
+		reviewImg.setReviewId(reviewId);
+	
+		log.info("reivewImage DTO : {} ", reviewImg.toString());
+	
+		return reviewImg;
 	}
+
 	
 
 	@Override
@@ -234,75 +191,65 @@ public ReviewImagesDTO imageIntoDTO(int reviewId, MultipartFile file) throws IOE
 		return lst;
 	}
 
-@Override
-@Transactional
-public boolean updateReview(ReviewDTO modifyReview) {
-	// 리뷰 수정
-	boolean result = false;
-	int reviewId=modifyReview.getReviewId();
-
-	if(revDAO.updateReview(modifyReview) == 1) {
-
-		if(modifyReview.getReviewImages() != null) {
-			for(ReviewImagesDTO img : modifyReview.getReviewImages()) {
-
-				int imageId=img.getImageId();
-
-				if (img.getFileStatus() == ReviewImagesStatus.INSERT) {
-					img.setReviewId(reviewId);
-					revDAO.insertImgs(img);
-
-				}else if(img.getFileStatus() == ReviewImagesStatus.DELETE) {
-					revDAO.deleteImgs(imageId);
+	@Override
+	@Transactional
+	public boolean updateReview(ReviewDTO modifyReview) {
+		// 리뷰 수정
+		boolean result = false;
+		int reviewId=modifyReview.getReviewId();
+	
+		if(revDAO.updateReview(modifyReview) == 1) {
+	
+			if(modifyReview.getReviewImages() != null) {
+				for(ReviewImagesDTO img : modifyReview.getReviewImages()) {
+	
+					int imageId=img.getImageId();
+	
+					if (img.getFileStatus() == ReviewImagesStatus.INSERT) {
+						img.setReviewId(reviewId);
+						revDAO.insertImgs(img);
+	
+					}else if(img.getFileStatus() == ReviewImagesStatus.DELETE) {
+						revDAO.deleteImgs(imageId);
+					}
 				}
 			}
+			result = true;
 		}
-		result = true;
+	
+		return result;
 	}
 
-	return result;
-}
-
-@Override
-public List<ReviewDTO> getMyReview(int userId,int page, int size) {
-	ReviewPageDTO paging = ReviewPageDTO.builder().userId(userId).page(page).size(size).build();
-	List<ReviewDTO> lst = revDAO.getMyReviews(paging);
-	for(ReviewDTO list : lst) {
-		log.info("{}",list.getCreateAtLocalDateTime().toString());
+		@Override
+		public List<ReserveDTO> getStatusByUserId(int userId) {
+			List<ReserveDTO> reserve= revDAO.getReserveByUserId(userId);
+			return reserve;
+		}
+		
+		@Override
+		public List<ReviewDTO> getUserReview(int userId) {
+			List<ReviewDTO> reviews =revDAO.getUserReviews(userId);
+			return reviews;
+		}
+		
+		@Override
+		public ReserveDTO getReserveInfoByReserveId(int reservationId) {
+			// 예약 번호로 예약 정보 가져오기
+			ReserveDTO reservation = revDAO.getReserveInfo(reservationId);
+			return reservation;
+		}
+		
+		@Override
+		public List<ReviewImagesDTO> selectReviewImagesByReviewId(int reviewId) {
+			// reviewId에 따른 reviewImages
+			return revDAO.filesByNo(reviewId);
+		}
+		        
+		@Override
+		public int checkImageExist(int imageId) {
+			// 이미지 파일이 존재하는지 확인
+			return revDAO.checkIfImageExist(imageId);
+		}
+	
+	
 	}
-	return lst;
-}
-
-@Override
-public List<ReserveDTO> getStatusByUserId(int userId) {
-	List<ReserveDTO> reserve= revDAO.getReserveByUserId(userId);
-	return reserve;
-}
-
-@Override
-public List<ReviewDTO> getUserReview(int userId) {
-	List<ReviewDTO> reviews =revDAO.getUserReviews(userId);
-	return reviews;
-}
-
-@Override
-public ReserveDTO getReserveInfoByReserveId(int reservationId) {
-	// 예약 번호로 예약 정보 가져오기
-	ReserveDTO reservation = revDAO.getReserveInfo(reservationId);
-	return reservation;
-}
-
-@Override
-public List<ReviewImagesDTO> selectReviewImagesByReviewId(int reviewId) {
-	// reviewId에 따른 reviewImages
-	return revDAO.filesByNo(reviewId);
-}
-        
-	@Override
-	public int checkImageExist(int imageId) {
-		// 이미지 파일이 존재하는지 확인
-		return revDAO.checkIfImageExist(imageId);
-	}
-
-
-}
